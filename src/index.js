8a0a26e6313c6feca202b8fd7744ab3545a01410
Yury Staravoitau <MorfinAll@mail.ru>
2017-09-17 12:41:52 +0300

    Product/feature (draft)implemented sumColumn function also disabled tests for simply working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = function multiply(first, second) {
+  "use strict"
 
   // упорядочиваем так чтобы при умножении столбиком верхнее число всегда было большей или равно разрядности 
   let x = "";
@@ -18,37 +19,77 @@ module.exports = function multiply(first, second) {
   let arr=[];
   for (let iY = y.length-1; iY >=0; iY--) {
     let str="";
-    console.log("iY= ",iY, " y[iY]= ",y[iY]);
+    //console.log("iY= ",iY, " y[iY]= ",y[iY]);
     let overflowDec=0;
     for (let iX = x.length-1; iX >=0; iX--) {
-      console.log("iX= ",iX," x[iX]= ",x[iX]);
+      //console.log("iX= ",iX," x[iX]= ",x[iX]);
       let multi=x[iX]*y[iY]+overflowDec;
-      console.log   ("DO IF  x[iX]*y[iY]= ",x[iX]*y[iY]," overflowDec= ",overflowDec);
+      //console.log   ("DO IF  x[iX]*y[iY]= ",x[iX]*y[iY]," overflowDec= ",overflowDec);
       if (multi >9) {
         overflowDec=Math.floor(multi/10);   
         str=str+String(multi%10);
         if (iX==0) str=str+overflowDec;
-        console.log (">9 IF  overflowDec= ",overflowDec," str= ", str," multi%10= ",multi%10);    
+        //console.log (">9 IF  overflowDec= ",overflowDec," str= ", str," multi%10= ",multi%10);    
       }
       else{
         str=str+String(multi);
-        console.log ("<=9 IF  overflowDec= ",overflowDec," str= ", str," multi= ",multi);   
+        //console.log ("<=9 IF  overflowDec= ",overflowDec," str= ", str," multi= ",multi);   
         overflowDec=0;
       }
     }
     arr.push(str);
   }
-  console.log ("arrReverse=   ",arr);
+  //console.log ("arrReverse=   ",arr);
 
   // делаем новый реверт массив полученных результатов (можно и без этого, но с этим будет нагляднее)
   function reverseStr(str) {
     return str.split("").reverse().join("");
   }
-  for (var i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let strNumber= arr[i];
     arr[i]=reverseStr(strNumber);
   }
-  console.log ("arr=   ",arr);
+  //console.log ("arr=   ",arr);
 
+  
+  function sumColumn (arrNumbStr){
+    let resultSumStr=arrNumbStr[0];
+    for (let iArr = 1; iArr < arrNumbStr.length; iArr++) { //i От 1 потому что  x=arrNumbStr[0]; 2 потому что мы текущее число складываем со след.
+      x=resultSumStr;
+      y=arrNumbStr[iArr];
+      if (x.length>y.length){  // делаем так чтобы у нас сверху было число больше разрядности.
+        let suBVal=x;
+        x=y;
+        y=subVal;
+      }
+      console.log ("iArr=", iArr," x= ",x," y= ",y);
+      let str="";
+      let overflowDec=0;
+      for (let i = 1; i <= x.length; i++) {  //i - посимвольное смещение от конца  i=1 потому что при смещении 0 у нас будут траблы с x[length]
+        let numbX=Number(x[x.length-i]);
+        let numbY=0;
+        if (i>y.length) numbY=0;     //  как только заканчиваются разряды меньшего числа подсовываем нули для сложения
+        else numbY=Number(y[y.length-i]);
+        let sum = numbX + numbY+overflowDec;
+        console.log("numbX= ", numbX," numbY= ",numbY," sum= ",sum);
+        if (sum >9) {
+          overflowDec=Math.floor(sum/10);   
+          str=str+String(sum%10);
+          if (i==x.length) str=str+overflowDec;  
+        }
+        else{
+          str=str+String(sum);
+          overflowDec=0;
+        }   
+      }
+      str=reverseStr(str);
+      console.log ("resultSumStr= ",str);
+      resultSumStr=str;
+    }
+    return resultSumStr;
+  }
+  arr=["123","456","789"];
+  console.log ("arr=   ",arr);
+  console.log("sumColumn(arr)=  ",sumColumn(arr));
 
 }
