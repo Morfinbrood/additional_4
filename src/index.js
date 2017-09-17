4446e08ee2eb8596ff70f1abf432438b65bbee1c
Yury Staravoitau <MorfinAll@mail.ru>
2017-09-17 14:25:44 +0300

    Product/refactoring: clean, little refact and added coments

--- src/index.js
+++ src/index.js
@@ -1,101 +1,81 @@
 module.exports = function multiply(first, second) {
   "use strict"
-
-  // упорядочиваем так чтобы при умножении столбиком верхнее число всегда было большей или равно разрядности 
-  let x = "";
-  let y = "";
-  if (first.lenght >= second.lenght) {
-    x = first;
-    y = second;
-  } else {
-    x = second;
-    y = first;
-  }
-
- // x="999";
- // y="888";
-
-  // получаем массив со строками 1-го этапа поциферного перемножения столбиком - результаты в строках и перевернуты, т.е. 123*11= ["321", "321"]
-  let arr=[];
-  let offseDecs="none";
-  for (let iY = y.length-1; iY >=0; iY--) {
-    let str="";
-    //console.log("iY= ",iY, " y[iY]= ",y[iY]);
-    let overflowDec=0;
-    for (let iX = x.length-1; iX >=0; iX--) {
-      //console.log("iX= ",iX," x[iX]= ",x[iX]);
-      let multi=x[iX]*y[iY]+overflowDec;
-      //console.log   ("DO IF  x[iX]*y[iY]= ",x[iX]*y[iY]," overflowDec= ",overflowDec);
-      if (multi >9) {
-        overflowDec=Math.floor(multi/10);   
-        str=str+String(multi%10);
-        if (iX==0) str=str+overflowDec;
-        //console.log (">9 IF  overflowDec= ",overflowDec," str= ", str," multi%10= ",multi%10);    
-      }
-      else{
-        str=str+String(multi);
-        //console.log ("<=9 IF  overflowDec= ",overflowDec," str= ", str," multi= ",multi);   
-        overflowDec=0;
-      }
-    }
-    if (offseDecs=="none") offseDecs="";
-    else offseDecs=offseDecs+"0";
-    //console.log(" str+offseDecs=",offseDecs+ str);
-    arr.push(offseDecs+str);
-  }
-  //console.log ("arrReverse=   ",arr);
-
-  // делаем новый реверт массив полученных результатов (можно и без этого, но с этим будет нагляднее)
+  //функция реверса строки
   function reverseStr(str) {
     return str.split("").reverse().join("");
   }
-  for (let i = 0; i < arr.length; i++) {
-    let strNumber= arr[i];
-    arr[i]=reverseStr(strNumber);
+  // упорядочиваем так чтобы при умножении столбиком верхнее число 'x' всегда было большей или равной разрядности 
+  let x = first;
+  let y = second;
+  if (y.length > x.length) {
+    let subVal = x;
+    x = y;
+    y = subVal;
   }
-  //console.log ("arr=   ",arr);
-
-  
-  function sumColumn (arrNumbStr){
-    let resultSumStr=arrNumbStr[0];
-    for (let iArr = 1; iArr < arrNumbStr.length; iArr++) { //i От 1 потому что  x=arrNumbStr[0]; 2 потому что мы текущее число складываем со след.
-      x=resultSumStr;
-      y=arrNumbStr[iArr];
-      //console.log("x.length= ",x.length," y.length= ",y.length);
-      if (y.length>x.length){  // делаем так чтобы у нас сверху было число больше разрядности.
-        let subVal=x;
-        x=y;
-        y=subVal;
+  //поциферно перемножаем 2-а стринг числа - как результат массив перевернутых чисел в string, т.е. 123*11= ["321", "321"]
+  let arrMulti = [];
+  let offseDecs = "none"; //величина смещения на десятки при умножении на цифру след десятка числа y
+  for (let iY = y.length - 1; iY >= 0; iY--) {
+    let str = "";
+    let overflowDec = 0;  //величина переполнения разряда т.е. 9*9=81 -> 8
+    for (let iX = x.length - 1; iX >= 0; iX--) {
+      let multi = x[iX] * y[iY] + overflowDec;
+      if (multi > 9) {
+        overflowDec = Math.floor(multi / 10);
+        str = str + String(multi % 10);
+        if (iX == 0) str = str + overflowDec; //чтобы не терять переполнение на послед цифре добавляем переполнение
+      } else {
+        str = str + String(multi);
+        overflowDec = 0;
       }
-      //console.log ("iArr=", iArr," x= ",x," y= ",y);
-      let str="";
-      let overflowDec=0;
-      for (let i = 1; i <= x.length; i++) {  //i - посимвольное смещение от конца  i=1 потому что при смещении 0 у нас будут траблы с x[length]
-        let numbX=Number(x[x.length-i]);
-        let numbY=0;
-        if (i>y.length) numbY=0;     //  как только заканчиваются разряды меньшего числа подсовываем нули для сложения
-        else numbY=Number(y[y.length-i]);
-        let sum = numbX + numbY+overflowDec;
-        //console.log("numbX= ", numbX," numbY= ",numbY," sum= ",sum);
-        if (sum >9) {
-          overflowDec=Math.floor(sum/10);   
-          str=str+String(sum%10);
-          if (i==x.length) str=str+overflowDec;  
+    }
+    if (offseDecs == "none") //1 раз не смещаем.
+      offseDecs = "";
+    else
+      offseDecs = offseDecs + "0";
+    arrMulti.push(offseDecs + str);
+  }
+  // делаем новый реверт массив полученных результатов
+  for (let i = 0; i < arrMulti.length; i++) {
+    let strNumber = arrMulti[i];
+    arrMulti[i] = reverseStr(strNumber);
+  }
+  // если бы была возможность было бы неплохо вынести это в отдельный модуль.
+  //Возможно это можно сделать даже в этом файле, но пока не умею.
+  x = 0;
+  y = 0;
+  function sumColumn(arrNumbStr) {
+    let resultSumStr = arrNumbStr[0];
+    //i От 1 потому что [0-ое] число мы уже всандалил в 'x'
+    for (let iArr = 1; iArr < arrNumbStr.length; iArr++) { 
+      let x = resultSumStr;
+      let y = arrNumbStr[iArr];
+      if (y.length > x.length) { // снова упорядочиваем
+        let subVal = x;
+        x = y;
+        y = subVal;
+      }
+      let str = "";
+      let overflowDec = 0;
+      for (let i = 1; i <= x.length; i++) { //i - посимвольное смещение от конца  i=1 потому что при смещении 0 у нас будут траблы с x[length]
+        let numbX = Number(x[x.length - i]);
+        let numbY = 0;
+        if (i <= y.length)
+          numbY = Number(y[y.length - i]);  //  как только заканчиваются разряды меньшего числа подсовываем нули для сложения
+        let sum = numbX + numbY + overflowDec;
+        if (sum > 9) {
+          overflowDec = Math.floor(sum / 10);
+          str = str + String(sum % 10);
+          if (i == x.length) str = str + overflowDec;
+        } else {
+          str = str + String(sum);
+          overflowDec = 0;
         }
-        else{
-          str=str+String(sum);
-          overflowDec=0;
-        }   
       }
-      str=reverseStr(str);
-      //console.log ("resultSumStr= ",str);
-      resultSumStr=str;
+      str = reverseStr(str);
+      resultSumStr = str;
     }
     return resultSumStr;
   }
-  //arr=["123","456","789"];
-  //console.log ("arr=   ",arr);
-  // console.log("sumColumn(arr)=  ",sumColumn(arr));
-  return sumColumn(arr);
-
+  return sumColumn(arrMulti);
 }
