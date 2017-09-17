a97477aaa7011d87ba0374c512c91a50d8748268
Yury Staravoitau <MorfinAll@mail.ru>
2017-09-17 10:06:39 +0300

    Product/feature: implemented reverseStr(str) and reverse all numbers in arr array

--- src/index.js
+++ src/index.js
@@ -11,16 +11,16 @@ module.exports = function multiply(first, second) {
     y = first;
   }
 
-  //console.log ("x= ",x, " y= ",y);
   x="999";
-  y="111";
+  y="888";
 
+  // получаем массив со строками 1-го этапа поциферного перемножения столбиком - результаты в строках и перевернуты, т.е. 123*11= ["321", "321"]
   let arr=[];
-  for (var iY = y.length-1; iY >=0; iY--) {
+  for (let iY = y.length-1; iY >=0; iY--) {
     let str="";
     console.log("iY= ",iY, " y[iY]= ",y[iY]);
     let overflowDec=0;
-    for (var iX = x.length-1; iX >=0; iX--) {
+    for (let iX = x.length-1; iX >=0; iX--) {
       console.log("iX= ",iX," x[iX]= ",x[iX]);
       let multi=x[iX]*y[iY]+overflowDec;
       console.log   ("DO IF  x[iX]*y[iY]= ",x[iX]*y[iY]," overflowDec= ",overflowDec);
@@ -38,7 +38,17 @@ module.exports = function multiply(first, second) {
     }
     arr.push(str);
   }
+  console.log ("arrReverse=   ",arr);
 
+  // делаем новый реверт массив полученных результатов (можно и без этого, но с этим будет нагляднее)
+  function reverseStr(str) {
+    return str.split("").reverse().join("");
+  }
+  for (var i = 0; i < arr.length; i++) {
+    let strNumber= arr[i];
+    arr[i]=reverseStr(strNumber);
+  }
   console.log ("arr=   ",arr);
+
 
 }
